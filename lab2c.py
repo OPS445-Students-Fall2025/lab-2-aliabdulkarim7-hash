fa5e944e57dbfeb470ad95ca9d40b2c1c4fa1623
Abdulkarim Ali <aliabdulkarim7@gmail.com>
2025-09-24 23:02:58 -0400

    Add lab2c solution with laboutput.txt

---
+++ lab2c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+name = sys.argv[1]          # first argument (string)
+age = int(sys.argv[2])      # second argument (convert to int)
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+
