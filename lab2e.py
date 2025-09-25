1c5da89ebf97b33c3ec397378184c61cf9a3f293
Abdulkarim Ali <aliabdulkarim7@gmail.com>
2025-09-24 23:17:56 -0400

    Complete lab2e with while loop and update laboutput

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Abdulkarim Ali
+# Author ID: aali394
+# Date Created: 2025/09/24
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
